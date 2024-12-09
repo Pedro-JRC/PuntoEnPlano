1b408fd283d9995544b29bad5f6fe09cc5e5ea81
Pedro J. Rosario <131802637+Pedro-JRC@users.noreply.github.com>
2024-12-08 21:17:39 -0400

    Add project files.

---
+++ PuntoEnPlano/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace PuntoEnPlano
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // VARIABLES
+            PuntoPlano puntoPlano = new PuntoPlano();
+            string digitaX;
+            string digitaY;
+            double x;
+            double y;
+
+            try
+            {
+                // SOLICITA LA COORDENADA X
+                Console.Write("Favor ingrese la coordenada X: ");
+                digitaX = Console.ReadLine();
+
+                // VALIDACIONES
+                if (double.TryParse(digitaX, out x))
+                {
+                    // SOLICITA LA COORDENADA Y
+                    Console.Write("\nFavor ingrese la coordenada Y: ");
+                    digitaY = Console.ReadLine();
+
+                    // VALIDACIONES
+                    if (double.TryParse(digitaY, out y))
+                    {
+                        // CARGA LAS CORRDENADAS
+                        puntoPlano.CargaValor(x, y);
+
+                        // MUESTRA EL RESULTADO
+                        Console.WriteLine("\nResultado:");
+                        puntoPlano.Resultado();
+                    }
+
+                }
+                else
+                {
+                    Console.WriteLine($"ERROR: Favor ingrese un valor valido");
+                }
+
+               
+            }
+            catch (Exception)
+            {
+                Console.WriteLine($"ERROR: Favor ingrese un valor valido");
+            }
+        }
+    }
+}
---
+++ PuntoEnPlano/PuntoPlano.cs
@@ -0,0 +1,44 @@
+﻿namespace PuntoEnPlano
+{
+    public class PuntoPlano
+    {
+        // PROPIEDADES DE COORDENADAS
+        private double x;
+        private double y;
+
+        // METODO CARGA LOS VALORES
+        public void CargaValor(double coordenadaX, double coordenadaY)
+        {
+            x = coordenadaX;
+            y = coordenadaY;
+        }
+
+        // METODO PARA VERIFICAR DONDE SE ENCUENTRA EL PUNTO
+        public string cuadrantePunto()
+        {
+            if (x > 0 && y > 0)
+                return "El punto se encuentra en el cuadrante [1]";
+            else if (x < 0 && y > 0)
+                return "El punto se encuentra en el cuadrante [2]";
+            else if (x < 0 && y < 0)
+                return "El punto se encuentra en el cuadrante [3]";
+            else if (x > 0 && y < 0)
+                return "El punto se encuentra en el cuadrante [4]";
+            else if (x == 0 && y == 0)
+                return "El punto se encuentra en el lugar de origen";
+            else if (x == 0)
+                return "El punto se encuentra en el eje Y";
+            else
+                return "El punto se encuentra en el eje X";
+
+
+
+        }
+
+        // METODO MUESTRA EL RESULTADO
+        public void Resultado()
+        {
+            Console.WriteLine(cuadrantePunto());
+        }
+    }
+}
